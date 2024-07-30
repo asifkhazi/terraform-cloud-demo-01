de228877e0ada3677a3942312ff444abbc4bf268
asifkhazi <116534238+asifkhazi@users.noreply.github.com>
2024-07-30 09:16:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo systemctl enable httpd
-sudo service httpd start
-echo "<h1> Welcome to terraform</h1>" | sudo tee /var/www/html/index.html
+apt-get update -y
+apt-get install apache2 -y
+systemctl enable apache2
+systemctl start apache2
+echo "<h1> <marquee>Welcome to world of Terraform</marquee></h1>" | sudo tee /var/www/html/index.html
