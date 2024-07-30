765e5eeb3d4fd9d02aa68b1af51c37d9ed80421e
asifkhazi <116534238+asifkhazi@users.noreply.github.com>
2024-07-30 09:14:14 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,12 +1,10 @@
 resource "aws_instance" "my_instance" {
-  ami                         = "ami-0cf0e376c672104d6"
+  ami                         = "ami-05b5693ff73bc6f84"
   instance_type               = var.instance_type
   count                       = 1
   associate_public_ip_address = true
-  subnet_id                   = "subnet-089f58d3a681e0693"
-  key_name                    = "ohio"
+  key_name                    = "hydrabad"
   user_data                   = file("install.sh")
-  vpc_security_group_ids      = ["sg-074ef07be41fc9239"]
   tags = {
     Name = "terraform-cloud-${count.index}"
   }
